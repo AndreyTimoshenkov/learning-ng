89547e96c96c7ad2647f058d494f69acd38afdf9
Andrey Timoshenkov <andreytmshnkv@gmail.com>
2023-09-28 12:16:16 +0300

    feat: add a static product card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderModule } from './components/header/header.module';
+import { CardModule } from './pages/product-list/card/card.module';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule, CardModule],
 	providers: [],
 	bootstrap: [AppComponent],
+	exports: [],
 })
 export class AppModule {}
