43ef36ef78988e62da9a543a1935250d305069c5
Andrey Timoshenkov <andreytmshnkv@gmail.com>
2023-10-02 17:24:25 +0300

    feat: add component card

--- src/app/pages/product-list/card/card.component.ts
+++ src/app/pages/product-list/card/card.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { IProduct } from '../product.interface';
+import { productMock } from '../product.mock';
 
 @Component({
-	selector: 'app-card',
-	templateUrl: './card.component.html',
-	styleUrls: ['./card.component.css'],
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-	onBuyClick(event: Event) {
-		event.stopPropagation();
-		console.log('onBuyClick');
-	}
+  onBuyClick(event: Event) {
+    event.stopPropagation();
+    console.log('onBuyClick');
+  }
+  public product: IProduct = productMock;
 }
