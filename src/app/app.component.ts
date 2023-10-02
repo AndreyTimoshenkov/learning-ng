8309e6010c95cb12aae1c063e413f2dbbb4cb741
Andrey Timoshenkov <andreytmshnkv@gmail.com>
2023-10-02 17:26:23 +0300

    feat: add component card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-	title = 'learning-ng';
-	onHeaderClick() {
-		console.log('onHeaderClick()');
-	}
-	logKeyEvent(event: Event) {
-		console.log(event);
-	}
+  title = 'learning-ng';
 }
