c5134fe51baf64b647555d904aaf665775e44554
Andrey Timoshenkov <andreytmshnkv@gmail.com>
2023-10-05 16:31:43 +0300

    fix: add the rest of fixes

---
+++ src/app/shared/products/app-config/app-config.mock.ts
@@ -0,0 +1,7 @@
+import { IApplicationConfig } from './app-config.interface';
+
+export const applicationConfigMock: IApplicationConfig = {
+  title: 'Angular-learnjs-110923',
+  shopIconSrc: '../../../favicon.ico',
+  headerColorPalete: 'primary',
+};
