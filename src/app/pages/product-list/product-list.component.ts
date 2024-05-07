f5b93c9f450d7970cb89db8bf38c8150220de28f
Andrey Timoshenkov <andreytmshnkv@gmail.com>
2024-05-07 18:24:08 +0300

    add hostBinding for scroll-dir, init the functionality

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
 // import { productMock } from '../../shared/products/product.mock';
 import { IProduct } from '../../shared/products/product.interface';
+import { ScrollDirective } from 'src/app/shared/products/scroll/scroll.directive';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, ScrollDirective],
 })
 export class ProductListComponent {
   // readonly products = productMock;
--- src/app/shared/products/scroll/scroll.directive.ts
+++ src/app/shared/products/scroll/scroll.directive.ts
@@ -1,27 +1,36 @@
 import { Directive, HostListener } from '@angular/core';
-import { fromEvent } from 'rxjs';
+// import { fromEvent } from 'rxjs';
 
 @Directive({
   selector: '[appScroll], appScroll',
   standalone: true,
+  // eslint-disable-next-line @angular-eslint/no-host-metadata-property
+  host: {
+    class: 'app-scroll-directive',
+  },
 })
 export class ScrollDirective {
   // constructor() {
-  //   this.productList = document.getElementById(
+  //   this.productList = document.getElementsByTagName(
   //     'app-product-list',
   //   );
+  //   console.log(this.productList);
   // }
+  // productList;
 
-  productList: HTMLCollectionOf<Element> =
-    document.getElementsByTagName('app-product-list');
-  @HostListener('document:scroll' /*, ['$event']*/)
-  onScroll() {
-    // if (this.productList) {
-    const scroll$ = fromEvent(document, 'scroll');
-    scroll$.subscribe(() => {
-      console.log('Scrolling detected!', this.productList);
-      // Perform actions based on the scroll event
-    });
+  @HostListener('scroll', ['$event.target'])
+  onScroll({
+    scrollTop,
+    clientHeight,
+    scrollHeight,
+  }: HTMLElement) {
+    // onScroll(event: HTMLElement) {
+    // const scroll$ = fromEvent(this.productList, 'scroll');
+    // scroll$.subscribe((event) => {
+    //   console.log(event.target.);
+    // });
+    // const scroll$ = fromEvent(target, 'scroll');
+    // scroll$.subscribe((event) => console.log(event));
+    console.log(scrollTop, clientHeight, scrollHeight);
   }
-  // }
 }
